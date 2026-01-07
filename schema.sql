099967e15719853aba3148ef49d1c020c71e4d12
alexisveloso25-png <alexis.veloso25@gmail.com>
2026-01-07 17:06:05 +0100

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -12,7 +12,7 @@ CREATE TABLE users (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     username TEXT UNIQUE NOT NULL,
     password TEXT NOT NULL,
-    role TEXT NOT NULL  -- admin ou user
+    role TEXT NOT NULL 
 );
 
 DROP TABLE IF EXISTS livres;
